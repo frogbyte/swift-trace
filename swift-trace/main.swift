e930a79e8a269c8e5a7b36ab64540be9870a3ef4
Kristopher Campbell <kris@frogbyte.net>
2016-04-05 22:06:49 -0700

    Initial Commit

---
+++ swift-trace/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-trace
+//
+//  Created by Kristopher Campbell on 4/5/16.
+//  Copyright © 2016 Kris Campbell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
